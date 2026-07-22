d9775e5c0a620db189bce356eda7e6748510fe57
Sanjana Goel <sanjana.goel@curefoods.in>
2026-07-22 11:22:30 +0530

    Point site config at the deployed Apps Script Web App URL

--- site/config.js
+++ site/config.js
@@ -1,2 +1,2 @@
 // Paste the Apps Script Web App deployment URL here (ends in /exec).
-const API_URL = 'https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbx3AsSkTuBAt6_97e9KZRW7TWKJXvqEIeDcSp2z3hY6L0azEk6EJDU7v-wcp3oKur_2/exec';
